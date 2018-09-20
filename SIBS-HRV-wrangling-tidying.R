0e668b39e7a024bba070a1e09f0c642e2d1810df
C.M. Bosma <bosma.colin@gmail.com>
2018-09-20 17:47:24 -0400

    updated SIBS RSA wrangling
    Included all RSA minutes and added "HRV" to functions to not read EDA files.

--- SIBS-HRV-wrangling-tidying.R
+++ SIBS-HRV-wrangling-tidying.R
@@ -1,10 +1,13 @@
+
+
+
 ### Psychophys HRV data wrangling and tidying code for SIBS
 ## Colin M. Bosma
 
 ## NOTES FOR RESEARCH ASSISTANTS
 ## -----------------------------------------------------------------------------
 
-# Excel needs to be closed while running this code. 
+# Excel needs to not be open while running this code. 
 # Do not edit code without contacting Colin first. 
 # Select lines of code, then press Ctrl + Enter keys to run 
 # Make sure all directories are correct and there are no typos in the file names
@@ -13,20 +16,21 @@
 ## REFERENCES
 ## -----------------------------------------------------------------------------
 
+
 # Tidyverse website 
-browseURL("http://tidyverse.org")
+# browseURL("http://tidyverse.org")
 
 # documentation for readxl pakage. Able to specify worksheets and columns
-browseURL("https://github.com/tidyverse/readxl")
+# browseURL("https://github.com/tidyverse/readxl")
 
 # tidyxl package documentation - import and manipulate awkward excel files
-browseURL("https://cran.r-project.org/web/packages/tidyxl/index.html")
+# browseURL("https://cran.r-project.org/web/packages/tidyxl/index.html")
 
 
 ## LOAD PACKAGES
 ## -----------------------------------------------------------------------------
 
-library(tidyverse)
+library(tidyverse) # This is the meta package for data wrangling
 library(readxl) # make sure it is the most recent version (v1.0.0)
 library(psych) # used for 'describe' function 
 library(haven) # for exporting data frame to an SPSS data file (.sav)
@@ -68,14 +72,14 @@ get_df <- function(data_path, file_match){
 
 # extracting values and creating dataframe using get_df() function
 baseline_df <- get_df(
-  data_path = "C:/Users/Mindware/Desktop/SIBS", 
+  data_path = "C:/Users/Mindware/Desktop/SIBS Mindware Data", 
   file_match = "HRVBaselineoutput.xlsx$"
     )
 
 print(baseline_df) # quick check
 
 # Add variable names 
-var_names <- c("id", "group", "basline_min1", "baseline_min2",
+var_names <- c("id", "group", "baseline_min1", "baseline_min2",
 				"baseline_min3", "baseline_min4", "baseline_min5",
 				"baseline_min6", "baseline_min7")
 			   
@@ -85,14 +89,16 @@ names(baseline_df) # check names
 
 head(baseline_df, 2) # check that variable names were assigned correctly
 
+View(baseline_df)
+
 
 ## MI: LOAD EXCEL SPREADSHEETS, EXTRACT VALUES, AND CREATE DATAFRAME
 ## -----------------------------------------------------------------------------
 
 # Extracting RSA values and creating a data frame
 os_df <- get_df(
-  data_path = "C:/Users/Mindware/Desktop/SIBS", 
-  file_match = "Ostracismoutput.xlsx$"
+  data_path = "C:/Users/Mindware/Desktop/SIBS Mindware Data", 
+  file_match = "HRVOstracismoutput.xlsx$"
     )
 
 print(os_df)
@@ -107,14 +113,15 @@ names(os_df) # check names
 
 head(os_df, 2)  # check that variable names were assigned correctly
 
+View(os_df)
 
 ## RECOVERY: LOAD EXCEL SPREADSHEETS, EXTRACT VALUES, AND CREATE DATAFRAME
 ## -----------------------------------------------------------------------------
 
 # Extracting RSA values and creating a data frame
 recovery_df <- get_df(
-  data_path = "C:/Users/Mindware/Desktop/AAE Mindware Data", 
-  file_match = "Recoveryoutput.xlsx$"
+  data_path = "C:/Users/Mindware/Desktop/SIBS Mindware Data", 
+  file_match = "HRVRecoveryoutput.xlsx$"
     )
 
 print(recovery_df) # quick check
@@ -130,6 +137,7 @@ names(recovery_df) # check names
            
 head(recovery_df, 2) # check that variable names were assigned correctly        
 
+View(recovery_df)
 
 ## COMBINE DATA FRAMES 
 ## -----------------------------------------------------------------------------
@@ -147,12 +155,13 @@ RSA_df <- dplyr::full_join(RSA_temp, recovery_df,
 head(RSA_df, 2) 
 dim(RSA_df) # Can cross reference # of observations and # of variables
 
+View(RSA_df) # Take a look at the data frame
 
 ## COMPUTING NEW VARIABLES
 ## -----------------------------------------------------------------------------
 
 # Save all variable names that should be numeric to a vector
-cols_num <- c("basline_min1", "baseline_min2", "baseline_min3", "baseline_min4",
+cols_num <- c("baseline_min1", "baseline_min2", "baseline_min3", "baseline_min4",
 			  "baseline_min5", "baseline_min6", "baseline_min7",
 			  "os_min1", "os_min2", "os_min3", "os_min4",
 			  "recovery_min1", "recovery_min2", "recovery_min3",
@@ -165,20 +174,24 @@ RSA_df[cols_num] <- sapply(RSA_df[cols_num], as.numeric)
 str(RSA_df) # check that RSA variables were converted to numeric
 
 # Compute mean RSA at baseline for last 5 minutes (min5-9)
-RSA_df$baseline_meanRSA <- rowMeans(RSA_df[c("baseline_min3",
+RSA_df$baseline_meanRSA <- rowMeans(RSA_df[c("baseline_min1",
+                                              "baseline_min2",
+                                              "baseline_min3",
                                               "baseline_min4",
                                               "baseline_min5",
                                               "baseline_min6",
                                               "baseline_min7")])
        
-# Compute mean RSA at mi for last 5 minutes (min3-7)                                       
-RSA_df$mi_meanRSA <- rowMeans(RSA_df[c("os_min1",
+# Compute mean RSA at mi for last 4 minutes (min3-7)                                       
+RSA_df$os_meanRSA <- rowMeans(RSA_df[c("os_min1",
                                         "os_min2",
                                         "os_min3",
-                                        "os_min4",)])
+                                        "os_min4")])
 
 # Compute mean RSA at recovery for last 5 minutes (min5-9)                                       
-RSA_df$recovery_meanRSA <- rowMeans(RSA_df[c("recovery_min3",
+RSA_df$recovery_meanRSA <- rowMeans(RSA_df[c("recovery_min1",
+                                             "recovery_min2",
+                                             "recovery_min3",
                                              "recovery_min4",
                                              "recovery_min5",
                                              "recovery_min6",
@@ -187,7 +200,7 @@ RSA_df$recovery_meanRSA <- rowMeans(RSA_df[c("recovery_min3",
 # Let's quickly check our work
 tail(names(RSA_df), 3) # check to see if variables were created 
 head(RSA_df$baseline_meanRSA, 5) 
-head(RSA_df$mi_meanRSA, 5) 
+head(RSA_df$os_meanRSA, 4) 
 head(RSA_df$recovery_meanRSA, 5) 
 
 
@@ -197,11 +210,10 @@ head(RSA_df$recovery_meanRSA, 5)
 # Structure and variable types
 str(RSA_df)
 
-# Summary of data frame
-describe(RSA_df)
 
 # View the data frame 
-View(RSA_df) # Review data table that is produced for issues
+# IMPORTANT: Review data table that is produced for issues
+View(RSA_df) 
 
 
 ## EXPORT DATA
@@ -212,14 +224,18 @@ View(RSA_df) # Review data table that is produced for issues
 # check current directory
 getwd() 
 # Set new working directory for saving .sav file
-setwd("insert-directory") 
+setwd("C:/Users/Mindware/Desktop/SIBS Mindware Data/SIBS SPSS Data Files") 
 
 # SPSS data frame
-write_sav(RSA_df, "HRVdata_date.sav")
+write_sav(RSA_df, "HRVdata_date_.sav")
 
 # .csv file for excel 
 write_csv(RSA_df, "SIBS_HRV_data.csv")
 
-# SPSS and .csv data files should be ready to go!
+# SPSS data file should be ready to go!
+
+
+# Write .csv for using this data frame to merge with other data sets using R
+
 
 
